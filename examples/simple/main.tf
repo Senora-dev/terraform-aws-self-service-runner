f31262d1bac57265036d9c5e931409c6e8fbe745
Shaked Braimok Yosef <shaked.braimok.yosef@gmail.com>
2024-03-15 08:00:02 +0200

    Update main.tf

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,5 +2,5 @@ module "create_secret_runner"{
     source = "Senora-dev/self-service-runner/aws"
     version = "~>1.0.0"
     action_identifier = "create-secrets"
-    buildspec_file = file("${path.module}/runners/sample.yaml")
+    buildspec_file = file("${path.module}/runners/buildspec.yaml")
 }
